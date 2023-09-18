b819c96217920d9686b6143d58d5645fdbc820c6
Fraser Morrison <frazemor@hotmail.com>
2023-09-17 23:06:17 -0400

    highlight tab button of active tab

--- src/lib/Router.svelte
+++ src/lib/Router.svelte
@@ -1,64 +1,64 @@
 <script lang="ts">
-    import GetPlaylist from "./GetPlaylist.svelte";
-    import PlaylistDisplay from "./PlaylistDisplay.svelte";
-    import MetaDataReader from "./SongReader.svelte";
-    import PlaylistLibrary from "./PlaylistLibrary.svelte";
-    import SongReader from "./SongReader.svelte";
+  import GetPlaylist from "./GetPlaylist.svelte";
+  import PlaylistDisplay from "./PlaylistDisplay.svelte";
+  import MetaDataReader from "./SongReader.svelte";
+  import PlaylistLibrary from "./PlaylistLibrary.svelte";
+  import SongReader from "./SongReader.svelte";
 
-    type Route = {
-        name: string;
-        path: string;
-        component: any;
-    };
+  type Route = {
+    name: string;
+    path: string;
+    component: any;
+  };
 
-    let routes: Route[] = [
-        {
-            name: "Playlist Library",
-            path: "/",
-            component: PlaylistLibrary,
-        },
-        {
-            name: "Get Playlist",
-            path: "/getplaylist",
-            component: GetPlaylist,
-        },
-        {
-            name: "Playlist Display",
-            path: "/playlist",
-            component: PlaylistDisplay,
-        },
-        {
-            name: "Song Reader",
-            path: "/songreader",
-            component: MetaDataReader,
-        },
-    ];
+  let routes: Route[] = [
+    {
+      name: "Playlist Library",
+      path: "/",
+      component: PlaylistLibrary,
+    },
+    {
+      name: "Get Playlist",
+      path: "/getplaylist",
+      component: GetPlaylist,
+    },
+    {
+      name: "Playlist Display",
+      path: "/playlist",
+      component: PlaylistDisplay,
+    },
+    {
+      name: "Song Reader",
+      path: "/songreader",
+      component: MetaDataReader,
+    },
+  ];
 
-    let currentRoute: Route = routes[0];
-    const selectTab = (event) => {
-        currentRoute = routes.find(
-            (route) => route.path === event?.detail?.tab
-        );
-    };
+  let currentRoute: Route = routes[0];
+  const selectTab = (event) => {
+    currentRoute = routes.find((route) => route.path === event?.detail?.tab);
+  };
 </script>
 
 <div>
-    {#each routes as route}
-        <button
-            on:click={() => {
-                currentRoute = route;
-            }}>{route.name}</button
-        >
-    {/each}
-    <!-- <SongReader /> -->
+  {#each routes as route}
+    <button
+      class={route === currentRoute ? "active" : ""}
+      on:click={() => {
+        currentRoute = route;
+      }}>{route.name}</button
+    >
+  {/each}
+  <!-- <SongReader /> -->
 </div>
 <div>
-    <svelte:component
-        this={currentRoute?.component}
-        on:tab-select={selectTab}
-    />
+  <svelte:component this={currentRoute?.component} on:tab-select={selectTab} />
 </div>
 
 <style>
-    /* your styles go here */
+  /* your styles go here */
+  .active {
+    background-color: #4caf50;
+    color: white;
+  }
 </style>
